f7c5449a1937e856b7230533bb1a8c10bda4383c
WillOli <william.ika@gmail.com>
2026-04-25 12:14:21 -0300

    first commit

---
+++ backend/src/test/java/com/grupoginga/GrupoGingaCapoeiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoginga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoGingaCapoeiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
